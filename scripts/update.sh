3c6d0fb62a63490e4ea30b06485ecc77d0f52ca9
arauser <alexander.rauser@gmail.com>
2025-01-27 12:48:01 +0400

    update fixes

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 set -e
 
 REPO_URL="https://github.com/superalgorithm/superalgorithm-cli.git"
-TEMP_DIR="/.tmp/superalgorithm-update"
+TEMP_DIR="/tmp/superalgorithm-update"
 INSTALL_DIR="$HOME/.superalgorithm"
 
 # Clone latest version
@@ -11,6 +11,9 @@ git clone "$REPO_URL" "$TEMP_DIR"
 
 # Update scripts
 cp -r "$TEMP_DIR/scripts/"* "$INSTALL_DIR/scripts/"
+
+# Set execute permissions for all scripts
+chmod -R +x "$INSTALL_DIR/scripts/"*.sh
 
 # Cleanup
 rm -rf "$TEMP_DIR"
